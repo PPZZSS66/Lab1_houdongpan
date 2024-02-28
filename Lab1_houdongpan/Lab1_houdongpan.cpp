9e14f1728c51de05a3712e6084f3f146ca0fcb2c
PPZZSS66 <148480085+PPZZSS66@users.noreply.github.com>
2024-02-27 23:13:46 -0500

    lab6

--- Lab1_houdongpan/Lab1_houdongpan.cpp
+++ Lab1_houdongpan/Lab1_houdongpan.cpp
@@ -1,47 +1,52 @@
 
 #include <iostream>
-#include <vector>
-#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 using namespace std;
 
-char encode(char inputChr,vector<char>& cipher) {
-    if (inputChr >= 65 && inputChr <= 90) {
-        return cipher[inputChr - 65];
-    }
-    else if (inputChr >= 97 && inputChr <= 122) { 
-        char upperLetter = inputChr - 32; 
-        char upperCode = cipher[upperLetter - 65]; 
-        return upperCode + 32; 
-    }
-    else { 
-        return inputChr;
-    }
-}
-
-string encodeString( string& input, vector<char>& cipher) {
-    string encoded = "";
-    for (char chr : input) {
-        encoded += encode(chr, cipher);
-    }
-    return encoded;
-}
-
 int main() {
-    vector<char> cipher = { 'V', 'F', 'X', 'B', 'L', 'I', 'T', 'Z', 'J', 'R', 'P', 'H', 'D', 'K', 'N', 'O', 'W', 'S', 'G', 'U', 'Y', 'Q', 'M', 'A', 'C', 'E' };
 
-    string text;
-    cout << "Input text to cypher: ";
-    getline(cin, text);
+    string background = "images1/backgrounds/winter.png";
+    string foreground = "images1/characters/yoda.png";
 
- 
-    if (text.length() > 128) {
-        cout << "Input is too long." << endl;
-        return 1;
+    Texture backgroundTex;
+    if (!backgroundTex.loadFromFile(background)) {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
     }
 
-    string encodedString = encodeString(text, cipher);
+    Texture foregroundTex;
+    if (!foregroundTex.loadFromFile(foreground)) {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
+    }
 
-    cout << "Encoded Message: " << encodedString<< endl;
+    Image backgroundImage;
+    backgroundImage = backgroundTex.copyToImage();
+    Image foregroundImage;
+    foregroundImage = foregroundTex.copyToImage();
 
-    return 0;
+    Vector2u sz = backgroundImage.getSize();
+
+    cout << sz.x << "," << sz.y << endl;
+    for (int y = 0; y < sz.y; y++) {
+        for (int x = 0; x < sz.x; x++) {
+            Color example = foregroundImage.getPixel(x, y);
+            if (example.g > 200 && example.r < 100 && example.b < 100) {
+                Color backgroundPixel = backgroundImage.getPixel(x, y);
+                foregroundImage.setPixel(x, y, backgroundPixel);
+            }
+        }
+    }
+
+    RenderWindow window(VideoMode(1024, 768), "Here is the out put");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(foregroundImage);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
 }
